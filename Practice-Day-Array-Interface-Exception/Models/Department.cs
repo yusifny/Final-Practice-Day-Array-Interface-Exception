76e0701f8d74e1b386b1cb8ccad075274a71c954
Yusif Nazarbayov <tu4rwlg2x@code.edu.az>
2022-05-02 14:38:56 +0400

    feat: Files uploaded

---
+++ Practice-Day-Array-Interface-Exception/Models/Department.cs
@@ -0,0 +1,78 @@
+﻿using Practice_Day_Array_Interface_Exception.CustomException;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_Day_Array_Interface_Exception.Models
+{
+    class Department
+    {
+        private int _employeeLimit;
+        public string Name { get; set; }
+        public int EmployeeLimit
+        {
+            get => _employeeLimit;
+            set
+            {
+                if (value > 0)
+                {
+                    _employeeLimit = value;
+                }
+            }
+        }
+
+        public Department(string name, int employeeLimit)
+        {
+            Name = name;
+            EmployeeLimit = employeeLimit;
+        }
+        Employee[] employees = new Employee[0];
+
+        public void AddEmployee(Employee employee)
+        {
+            if (employees.Length < EmployeeLimit)
+            {
+                Array.Resize(ref employees, employees.Length + 1);
+                employees[employees.Length - 1] = employee;
+                Console.WriteLine($"{employee}\n [i] Has been added\n-------------------");
+
+            }
+            else
+            {
+                throw new CapacityLimitException("Failed to add the employee!\nThe Department Limit has been reached");
+            }
+        }
+
+        public Employee this[int i]
+        {
+            get
+            {
+                if (i < employees.Length)
+                {
+                    return employees[i];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                if (i < employees.Length)
+                {
+                    employees[i] = value;
+                    return;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        internal void AddEmployee(object employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Practice-Day-Array-Interface-Exception/Program.cs
@@ -0,0 +1,47 @@
+﻿using Practice_Day_Array_Interface_Exception.CustomException;
+using Practice_Day_Array_Interface_Exception.Models;
+using System;
+
+namespace Practice_Day_Array_Interface_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            #region Employees
+
+                Employee employee1 = new Employee("Loghman", 20, 350.31);
+
+                Employee employee2 = new Employee("Yusif", 21, 289.00);
+
+                Employee employee3 = new Employee("Murad", 20, 150.69);
+
+                Employee employee4 = new Employee("Elon", 27, 1500.69);
+
+            #endregion
+
+            #region Departments
+
+
+                Department department = new Department("Software Development", 3);
+
+            try
+            {
+                department.AddEmployee(employee1);
+                department.AddEmployee(employee2);
+                department.AddEmployee(employee3);
+                ////also run the program enabling the following line of code to see thrown exception.
+                //department.AddEmployee(employee4);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            #endregion
+
+        }
+    }
+}
